642225dae0b0fa4172a1e371082466f6ab87800b
Lauris <91956740+Laurisdid@users.noreply.github.com>
2022-05-23 14:32:09 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import './App.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Books from './Components/011/Books';
+import axios from 'axios';
 
 
   
 
 function App() {
+
     const [count, setCount] = useState(1);
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        console.log('YES'); // REDY!
+        axios.get('http://in3.dev/knygos/')
+        .then(res => {
+            console.log(res.data);
+            books.map(i=><div key={books}></div>)
+        })
+    }, []);
+
+    
     return (
         <div className="App">
           <header className="App-header">
            <h1>{count}</h1>
-           <button>+1</button>
+           <button onClick={() => setCount(c => c + 1)}>+1</button>
+           <Books books={books}></Books>
+     
           </header>
         </div>
       );

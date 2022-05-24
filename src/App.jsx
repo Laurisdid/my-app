7aa38c6570ae98385cfe9980cceecfcd72549c73
Lauris <91956740+Laurisdid@users.noreply.github.com>
2022-05-24 14:13:32 +0300

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,57 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.scss';
 
 function App() {
 
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(null);
 
     const mano = useRef(0);
     const panda = useRef();
+
+    useEffect(() => {
+        setCount(parseInt(localStorage.getItem('count') ?? 0));
+    }, []);
+
+    useEffect(() => {
+        if (null === count) {
+            return;
+        }
+        localStorage.setItem('count', count);
+    }, [count]);
+
+    
 
     const add = () => {
         setCount(c => c + 1);
         mano.current = mano.current + 3;
         console.log(mano.current);
         // const p = document.querySelector('#panda');
-     //   const p = panda.current;
-        // console.log(p.dataset.panda);
+        const p = panda.current;
+        console.log(p.dataset.panda);
     }
-    const addCat=()=>{
-        localStorage.setItem('katinukas',JSON.stringify(8));
+
+    const addCat = () => {
+        localStorage.setItem('katinukas', JSON.stringify(['Murka', 'Pilkis']));
     }
-    const getCat=()=>{
-        console.log(JSON.parse(localStorage.getItem(['katinukas','pilkis'])))
+
+    const getCat = () => {
+        console.log(JSON.parse(localStorage.getItem('katinukas')));
     }
-    const remCat =()=>{
+
+    const remCat = () => {
         localStorage.removeItem('katinukas');
     }
+
 
     return (
         <div className="App">
           <header className="App-header">
            <h1>useRef LocalStorage {count}</h1>
            <button onClick={add}>+1</button>
+           <button onClick={addCat}>Add Cat</button>
+           <button onClick={getCat}>Get Cat</button>
+           <button onClick={remCat}>Remove Cat</button>
            <div ref={panda} data-panda="miega"></div>
-           <button onClick={addCat} cat>add cat</button>
-           <button onClick={getCat} cat>get cat</button>
-           <button onClick={remCat} cat>rem cat</button>
           </header>
         </div>
       );
